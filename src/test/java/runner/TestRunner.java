3685f4203425c0b05a9a3c9af2f1e858ff816d3b
vishnugoddumarri <vishnu.goddumarri@zenq.com>
2020-12-22 23:21:36 +0530

    TestNG Update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,15 +3,49 @@ package runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-@RunWith(Cucumber.class)
+
 @CucumberOptions(
         features = "C:\\Users\\vishnu\\IdeaProjects\\BigBasket\\src\\test\\java\\features\\login.feature"//the path of the feature files
-        , glue={"stepdefination"}
-       // format= {"Report","html:test-outout"}
-)
+        , glue={"stepdefination"},
+        tags = {"~@Ignore"},
+        format= {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/cucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt"
+        },plugin = "json:target/cucumber-reports/cucumberTestReport.json")
 
 public class TestRunner {
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception{
+        testNGCucumberRunner= new TestNGCucumberRunner(this.getClass());
+
+    }
+
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature){
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+
+    @DataProvider
+    public Object[][] features(){
+        return testNGCucumberRunner.provideFeatures();
+    }
+
+    @AfterClass
+   public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+
+    }
 
 }
---
+++ target/cucumber-reports/cucumber-pretty/report.js
@@ -0,0 +1,136 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/vishnu/IdeaProjects/BigBasket/src/test/java/features/login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Big Basket  Add to Cart Feature",
+  "description": "",
+  "id": "big-basket--add-to-cart-feature",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Big Basket Add to Cart Test Scenario",
+  "description": "",
+  "id": "big-basket--add-to-cart-feature;big-basket-add-to-cart-test-scenario",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user clicks on search button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 7,
+  "name": "user select By Availability Filter",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user select item Category Filter",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user select by Brand",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user gets Respective product names",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "user clicks on add to cart button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 12,
+  "name": "user clicks on Cart button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "user removes the items in cart",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 14,
+  "name": "user closes the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefination.user_is_already_on_Home_Page()"
+});
+formatter.result({
+  "duration": 13869355329,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefination.user_clicks_on_search_button()"
+});
+formatter.result({
+  "duration": 3197841380,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefination.user_select_By_Availability_Filter()"
+});
+formatter.result({
+  "duration": 1011561325,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefination.user_select_item_Category_Filter()"
+});
+formatter.result({
+  "duration": 549648167,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefination.userSelectByBrand()"
+});
+formatter.result({
+  "duration": 486374683,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefination.userGetsRespectiveProductNames()"
+});
+formatter.result({
+  "duration": 3434589142,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefination.user_clicks_on_add_to_cart_button()"
+});
+formatter.result({
+  "duration": 2618357716,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefination.user_clicks_on_Cart_button()"
+});
+formatter.result({
+  "duration": 5272408311,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefination.userRemovesTheItemsInCart()"
+});
+formatter.result({
+  "duration": 936183577,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefination.userClosesTheBrowser()"
+});
+formatter.result({
+  "duration": 2790407420,
+  "status": "passed"
+});
